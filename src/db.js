e45a0f557d0382cce15b28ec4176be9354360468
Jessica Amy <62581609+dajesso@users.noreply.github.com>
2025-08-09 04:04:31 +0930

    Update db.js

--- src/db.js
+++ src/db.js
@@ -23,7 +23,7 @@ const mongoose = require('mongoose');
 
 const connect = async () => {
   try {
-    await mongoose.connect("mongodb+srv://db-mongodb-syd1-49354-365d51c3.mongo.ondigitalocean.com/admin", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
