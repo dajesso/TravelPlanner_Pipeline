fc2bbd160766a53252ac7a51f92b4b49fb20dad1
Jessica Amy <62581609+dajesso@users.noreply.github.com>
2025-08-09 15:40:10 +0930

    Update index.js

--- src/index.js
+++ src/index.js
@@ -32,9 +32,11 @@ app.use(category_routes);
 
 // start the given server on the given port
 
+
 if (require.main === module) {
-  app.listen(process.env.PORT || 8000, () => {
-    console.log('Server started');
+  app.listen(port, async () => {
+    console.log(`Example app listening on port ${port}`);
+    connect();
   });
 }
 
